853a8c3b1e072e618183de26b714d8189cca4bcb
LEEN1CK <nick9797349@gmail.com>
2026-05-10 23:25:05 +0900

    Rename shop/src/main/java/com/example/MyShop/order/Order.java to shop/src/main/java/com/example/MyShop/order/entity/Order.java

---
+++ shop/src/main/java/com/example/MyShop/order/entity/Order.java
@@ -0,0 +1,53 @@
+package com.example.MyShop.order;
+
+import com.example.MyShop.member.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "order_data")
+    private LocalDateTime orderDate;
+
+    @Column(name = "total_price")
+    private int totalPrice;
+
+    @Column(name = "point_used")
+    private int pointUsed;
+
+    @Column(name = "cash_amount")
+    private int cashAmount;
+
+    @Column(name = "status", length = 25)
+    private String status;
+
+    public Order(Member member, int totalPrice, int pointUsed, int cashAmount) {
+        this.member = member;
+        this.orderDate = LocalDateTime.now();
+        this.totalPrice = totalPrice;
+        this.pointUsed = pointUsed;
+        this.cashAmount = cashAmount;
+        this.status = "ORDERED";
+    }
+
+    public void cancel() {
+        this.status = "CANCELED";
+    }
+}
